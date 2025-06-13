eb7b230284ace54656ffb27b5c3f544fde246985
JiahaoChow <plus_chou@foxmail.com>
2025-06-13 08:02:40 +0800

    version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'midas'
 copyright = '2024, labomics'
 author = 'labomics'
-release = '0.1.3'
+release = '0.1.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
